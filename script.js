c5d1a883e8ea3180bb04ac0ed44f26c470054cec
Harsh <harshpandey2060@gmail.com>
2023-11-17 23:41:53 +0530

    Search bar search the contact using Array object

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
+let contactArrays = [];
+
 function addContact() {
 
-  const conList = document.getElementById("conList");
-
-  let contactArrays = [];
-
-
-
   if (Fname.value.trim() !== "") {
-    const contacts = document.createElement("li");
-    contacts.className = "contacts";
 
     const contactlist =
     {
@@ -16,43 +10,73 @@ function addContact() {
       Lname: document.getElementById("Lname").value.trim(),
       Phone: document.getElementById("Phone").value.trim(),
     }
-
     contactArrays.push(contactlist);
+    displayContacts(contactlist)
 
-
-    const FnameText = document.createElement("span");
-    FnameText.textContent = contactlist.Fname;
-    const LnameText = document.createElement("span");
-    LnameText.textContent = contactlist.Lname
-    const num = document.createElement("span");
-    num.textContent = contactlist.Phone;
-
-    const removeButton = document.createElement("button");
-    removeButton.textContent = "Delete";
-    removeButton.onclick = function () {
-      conList.removeChild(contacts);
-    };
-
-    
-    contacts.appendChild(FnameText);
-    contacts.appendChild(LnameText);
-    contacts.appendChild(num);
-    contacts.appendChild(removeButton);
-
-    conList.appendChild(contacts);
-    Fname.value = ""; // Clear the input field after adding the task
-    Lname.value = ""; // Clear the input field after adding the task
-    Phone.value = ""; // Clear the input field after adding the task
   }
+
 }
 
 
+
+function displayContacts(contactlist) {
+
+  console.log(contactlist)
+  const conList = document.getElementById("conList");
+  const contacts = document.createElement("li");
+  contacts.className = "contacts";
+
+  const FnameText = document.createElement("span");
+  FnameText.textContent = contactlist.Fname;
+  const LnameText = document.createElement("span");
+  LnameText.textContent = contactlist.Lname
+  const num = document.createElement("span");
+  num.textContent = contactlist.Phone;
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Delete";
+  removeButton.onclick = function () {
+    conList.removeChild(contacts);
+  };
+  contacts.appendChild(FnameText);
+  contacts.appendChild(LnameText);
+  contacts.appendChild(num);
+  contacts.appendChild(removeButton);
+
+  conList.appendChild(contacts);
+
+}
 
 function searchContacts() {
   const searchInput = document.getElementById("searchInput");
   const searchTerm = searchInput.value.trim().toLowerCase();
 
-  const filteredContacts = contactArrays.filter(contact => contact.name.toLowerCase().includes(searchTerm));
+  if (searchTerm.value !== "") {
+    const filteredContacts = contactArrays.filter(contact => contact.Fname.toLowerCase().includes(searchTerm));
 
-  updateContactList(filteredContacts);
+    const contactlist =
+    {
+      Fname: filteredContacts.map(obj => obj.Fname),
+      Lname: filteredContacts.map(obj => obj.Lname),
+      Phone: filteredContacts.map(obj => obj.Phone)
+    }
+
+    if (contactlist.Fname.length != 0) {
+
+      console.log(contactlist)
+      const contactList = document.getElementById("conList");
+      contactList.innerHTML = "";
+      displayContacts(contactlist)
+    }
+
+    else {
+      alert("No Contact available")
+    }
+
+  }
+
+  else{
+    alert("Please Enter some values")
+  }
+
 }
