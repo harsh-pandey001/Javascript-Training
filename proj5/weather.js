775993f6aae43ac4723bd9f5716c27c826e01c08
Harsh <harshpandey2060@gmail.com>
2023-12-26 23:13:17 +0530

    weather app

---
+++ proj5/weather.js
@@ -0,0 +1,83 @@
+
+function place(){ 
+ let visit = document.getElementById('Place').value
+console.log(visit)
+
+
+let p = fetch(`http://api.weatherapi.com/v1/current.json?key=a18d009f33eb473d9c9175805231912&q=${visit}&aqi=no`);
+
+
+
+p.then(response =>{
+    if (!response.ok) {
+        console.log("There is some error on fetching the data")
+    }
+    console.log(response.status);
+    return response.json();
+}).then(data =>{
+    console.log(data);
+    updateHtml(data)
+})
+}
+
+
+function updateHtml(data) {
+    const dataContainer = document.getElementById('data-container');
+    dataContainer.innerHTML = '';
+
+    // Extract data from the API response
+    const location = data.location;
+    const currentWeather = data.current;
+
+    // Create elements to display location information
+    const locationCard = document.createElement('div');
+    locationCard.classList.add('location-card');
+
+    const cityNameElement = document.createElement('h2');
+    cityNameElement.textContent = location.name + ', ' + location.region + ', ' + location.country;
+
+    const coordinatesElement = document.createElement('p');
+    coordinatesElement.textContent = `Latitude: ${location.lat}, Longitude: ${location.lon}`;
+
+    locationCard.appendChild(cityNameElement);
+    locationCard.appendChild(coordinatesElement);
+
+    // Create elements to display current weather information
+    const weatherCard = document.createElement('div');
+    weatherCard.classList.add('weather-card');
+
+    const temperatureElement = document.createElement('p');
+    temperatureElement.textContent = `Temperature: ${currentWeather.temp_c} °C`;
+
+    const lastUpdatedElement = document.createElement('p');
+    lastUpdatedElement.textContent = `Last Updated: ${currentWeather.last_updated}`;
+
+    // Add more elements as needed based on your API response
+
+    weatherCard.appendChild(temperatureElement);
+    weatherCard.appendChild(lastUpdatedElement);
+
+    // Append both location and current weather cards to the data container
+    dataContainer.appendChild(locationCard);
+    dataContainer.appendChild(weatherCard);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {
+// "location": { "name": "Mumbai", "region": "Maharashtra", "country": "India", "lat": 18.98, "lon": 72.83, "tz_id": "Asia/Kolkata", "localtime_epoch": 1703170629, "localtime": "2023-12-21 20:27" },
+
+// "current": { "last_updated_epoch": 1703169900, "last_updated": "2023-12-21 20:15", "temp_c": 29.0, "temp_f": 84.2, "is_day": 0, "condition": { "text": "Overcast", "icon": "//cdn.weatherapi.com/weather/64x64/night/122.png", "code": 1009 }, "wind_mph": 3.8, "wind_kph": 6.1, "wind_degree": 290, "wind_dir": "WNW", "pressure_mb": 1013.0, "pressure_in": 29.91, "precip_mm": 0.0, "precip_in": 0.0, "humidity": 48, "cloud": 50, "feelslike_c": 31.7, "feelslike_f": 89.0, "vis_km": 2.5, "vis_miles": 1.0, "uv": 1.0, "gust_mph": 14.6, "gust_kph": 23.4 }
+//  }
--- script2.js
+++ script2.js
@@ -1,23 +1,53 @@
-let promis =  fetch("http://api.weatherapi.com/v1/current.json?key=a18d009f33eb473d9c9175805231912&q=London&aqi=no")
-promis.then(response =>{
-  console.log(response.ok);
-  return response.json()
-}).then(data => {
-    console.log(data);
- })
+// let promis =  fetch("http://api.weatherapi.com/v1/current.json?key=a18d009f33eb473d9c9175805231912&q=London&aqi=no")
+// promis.then(response =>{
+//   console.log(response.ok);
+//   return response.json()
+// }).then(data => {
+//     console.log(data);
+//  })
 
 // hoisting
 // console.log(a)
+// console.log(new Rectangle(5, 8).area());
+
 // var a = 55;
-
-//  hit();
-//  hit1()
-//  function hit (){
-//  console.log("hello world");
-//  }
- 
-//  let a =  hit1() {
-
-//  }
+// // console.log(b);
+// hit();
 
 
+
+// hit1()
+// function hit() {
+//     console.log("hello world");
+// }
+
+
+//  class expresssions
+
+// var Rectangle = class {
+//     constructor(height, width) {
+//       this.height = height;
+//       this.width = width;
+//     }
+//     area() {
+//       return this.height * this.width;
+//     }
+//   };
+  
+//   console.log(new Rectangle(5, 8).area());
+
+
+
+// Runtime Error Example
+let x = 10;
+let y = 0;
+let result;
+
+
+result = x / y; // Division by zero
+console.log(result);
+// try {
+
+// } catch (error) {
+//   console.error("Runtime Error:", error.message);
+// }
