0acd42efa10d53373bde67c7493153b03853cbcf
Harsh <harshpandey2060@gmail.com>
2024-01-22 17:19:38 +0530

    chnages

--- JavaScript Practised code/try_catch.js
+++ JavaScript Practised code/try_catch.js
@@ -1,8 +1,6 @@
 // The try...catch statement is comprised of a try block and either a catch block, a finally block, or both. The code in the try block is executed first, and if it throws an exception, the code in the catch block will be executed. The code in the finally block will always be executed before control flow exits the entire construct.
 
-
-// syntax
-
+// SYNTAX
 // try {
 //     tryStatements
 //   } catch (exceptionVar) {
@@ -28,14 +26,14 @@
 // a = 99;
 // console.log(a)
 // }catch(e){
- 
+
 //     console.log(e.message)
 
 // }
 
 // try {
 //     throw new TypeError("oops");
-    
+
 //   } catch ({ name, message }) {
 //     console.log(name); // "TypeError"
 //     console.log(message); // "oops"
@@ -48,26 +46,68 @@
 
 //    return prom.json;
 // }
-function isValidJSON(text) {
-    try {
-      JSON.parse(text);
-      return true;
-    } catch {
-      return false;
-    }
-  }
 
 
+// let JSONobj = {
+//     Name: "Harsh",
+//     Designation: "Associate Software Engeener Trainee",
+//     Company: "CubeXO",
+//     Experiance: "2 months",
+//     College: "School of Computer Science & IT"
+// }
+
+// JSON.parse(JSONobj)
+// console.log(JSONobj);
+
+
+// function isValidJSON(JSONobj) {
+//     try {
+//         // JSON.parse(JSONobj);
+//         console.log(JSON.stringify(JSONobj));
+
+//         return true;
+//     } catch (error) {
+//         // console.log(error);
+//         return false;
+//     }
+// }
+
+// console.log(isValidJSON(JSONobj));
+
+
+
+//Error handling with .catch()
 function json_call() {
-    fetch("./test.json").then((response)=>{
-        return response.json()
-    }).then(function(data){
-        console.log(data)
-        isValidJSON(data)
+    fetch("http://api.weatherapi.com/v1/current.json?key=a18d009f33eb473d9c9175805231912&q=London&aqi=no").then((response) => {
+        if (!response.ok) {
+            console.log(response.status);
+            
+            return response.json()
+        }
+    }).catch(error => {
+        console.error('An error occurred:', error)
     })
 }
 
+
 json_call()
+
+// async function json_call() {
+//     let q = await fetch("./test.json");
+//     let ret = await q.json();
+
+//     console.log(ret);
+//     return ret;
+// }
+
+// const prom = async () => {
+//     let response = await json_call();
+//     console.log(response);
+
+// }
+
+// prom()
 
 
 //   isValidJSON(json_call())
+
